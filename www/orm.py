5b3783aa36538e9adb75769014c922923ff19653
lazy928 <lazyboy928@gmail.com>
2020-11-26 00:07:06 +0800

    add orm.py

--- www/orm.py
+++ www/orm.py
@@ -1,2 +1,21 @@
 # -*- coding: utf-8 -*-
+import logging, aiomysql
+
+# 创建连接池
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    # 全局变量__pool 存储，默认为utf8
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host = kw.get('host', 'localhost'),
+        port = kw.get('port',3360),
+        user = kw('user'),
+        password = kw('password'),
+        db = kw('db'),
+        charset = kw.get('charset', 'utf8'),
+        autocommit = kw.get('autocommit', True),
+        maxsize = kw.get('maxsize', 10),
+        minsize = kw.get('minsize',1),
+        loop = loop
+    )
 
